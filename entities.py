65cc0055afc349a863ebef714ba6f756a97e1348
Damian <d.pagowski@gmail.com>
2019-09-23 17:57:22 +0700

    added gitignore. removed unnecessary files

--- entities.py
+++ entities.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
@@ -8,3 +9,14 @@ class Restaurant(Base):
     __tablename__ = "restaurant"
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
